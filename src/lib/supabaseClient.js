99c8a4c5b1dfb1fff9c854409afe8f36fe75b66b
User26ts <tommasoschiavon26@gmail.com>
2026-04-27 03:08:38 +0200

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  "PROJECT_URL",
-  "sb_publishable_KEY"
+  "https://YOUR_PROJECT_URL.supabase.co",  // Sostituisci con la tua URL
+  "sb_publishable_YOUR_PUBLISHABLE_KEY"   // Sostituisci con la tua publishable key
 )
 
 export default supabase
