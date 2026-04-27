ddd5b7795e1a91da87efb3de022fb3a4f1c29c9b
User26ts <tommasoschiavon26@gmail.com>
2026-04-28 00:05:33 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = { title: 'Circlo Refresh' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="it">
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  )
+}
