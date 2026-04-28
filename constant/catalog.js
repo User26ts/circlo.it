39c9d68c81d2120168437f22955d285dee4d452f
User26ts <tommasoschiavon26@gmail.com>
2026-04-28 23:33:25 +0200

    Create catalog.js

---
+++ constant/catalog.js
@@ -0,0 +1,42 @@
+export const AFFINITY_CATALOG = {
+  musica: {
+    classica: {
+      barocca: ["Bach", "Vivaldi", "Handel", "Scarlatti", "Corelli", "Purcell"],
+      romantica: ["Chopin", "Rachmaninov", "Liszt", "Beethoven", "Schubert", "Brahms", "Tchaikovsky"],
+      modernismo: ["Stravinsky", "Debussy", "Ravel", "Mahler", "Shostakovich"],
+      contemporanea: ["Einaudi", "Max Richter", "Philip Glass", "Jóhannsson"]
+    },
+    urban_pop: {
+      trap_it: ["Sfera Ebbasta", "Lazza", "Marracash", "Guè", "Tedua", "Tony Effe", "Anna"],
+      rap_usa: ["Travis Scott", "Drake", "Kendrick Lamar", "Kanye West", "Playboi Carti"],
+      pop: ["Taylor Swift", "Ariana Grande", "The Weeknd", "Harry Styles", "Billie Eilish"]
+    },
+    techno_rave: {
+      hard_techno: ["Klangkuenstler", "Kobosil", "Sara Landry", "999999999"],
+      melodic: ["Anyma", "Tale of Us", "Solomun", "Artbat"]
+    }
+  },
+  scienze_e_sapere: {
+    scienze_pure: ["Matematica Pura", "Fisica Teorica", "Astrofisica", "Chimica Organica", "Biologia Molecolare"],
+    ingegneria: ["Meccanica", "Aerospaziale", "Informatica/AI", "Robotica", "Bioingegneria"],
+    umanistiche: ["Psicologia Clinica", "Sociologia", "Antropologia", "Archeologia", "Linguistica"]
+  },
+  filosofia_e_letteratura: {
+    correnti: ["Esistenzialismo", "Stoicismo", "Nihilismo", "Idealismo", "Fenomenologia", "Epicureismo"],
+    letteratura: ["Classici Greci/Latini", "Romanticismo", "Decadentismo", "Realismo Magico", "Poesia Moderna"],
+    autori_culto: ["Nietzsche", "Kafka", "Dostoevskij", "Pessoa", "Camus", "Orwell", "Hesse"]
+  },
+  estetica_vibe: {
+    cores: ["Dreamcore", "Frutiger Aero", "Vaporwave", "Liminal Spaces", "Cyberpunk", "Weirdcore"],
+    stili: ["Old Money", "Dark Academia", "Cottagecore", "Minimalismo", "Streetwear", "Grunge"]
+  },
+  cibo_e_gusto: {
+    cucine: ["Italiana (Tradizionale)", "Giapponese/Sushi", "Fusion", "Messicana", "Coreana", "Mediterranea"],
+    esperienze: ["Fine Dining", "Street Food", "Vini Naturali", "Mixology", "Pasticceria Tecnica"]
+  },
+  sport_e_corpo: {
+    agonismo: ["Calcio", "Basket", "Tennis", "Padel", "Volley", "Nuoto"],
+    outdoor: ["Arrampicata", "Surf", "Skateboarding", "Sci/Snowboard", "Trekking"],
+    mind_body: ["Bodybuilding", "Crossfit", "Yoga", "Pilates", "Arti Marziali"]
+  }
+};
